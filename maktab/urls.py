476c09255074d74c5563be80b38879462aafba2e
Iqbola <Alisherovna-Iqbola0o1@gmail.com>
2025-12-26 16:42:05 +0500

    Barcha views.py va urls.py lar Hammasi Tugallandi !

--- maktab/urls.py
+++ maktab/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='home'),
+    path('', views.maktab_list, name='maktab_list'),
+    path('direktorlar/', views.direktor_list, name='direktor_list'),
 ]
--- universitet/views.py
+++ universitet/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from .models import Universitet, Direktor
+
+def universitet_list(request):
+    universitetlar = Universitet.objects.all()
+    return render(request, "universitet_list.html", {"universitetlar": universitetlar})
+
+def direktor_list(request):
+    direktorlar = Direktor.objects.all()
+    return render(request, "direktor_list.html", {"direktorlar": direktorlar})
