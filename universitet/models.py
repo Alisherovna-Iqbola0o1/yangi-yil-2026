c0a1b31e1c7e6a158f77514b8bccd417c0a5e0b5
Diyorbek <irmatovdiyorbek36@gmail.com>
2025-12-26 16:06:47 +0500

    ...

--- universitet/models.py
+++ universitet/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django import User
 
 # Create your models here.
+# 2: Universitet class  name, manzil, oquvchilar soni, maktab-ochilgan-vaqt, yaratilgan vaqt  Direktor class 
+# forinkey user, oylik, ishga-kelgan-vaqt, yaratilgan vaqt
+
+class Universitet(models.Model):
+    name = models.CharField(max_length=255)
+    manzil = models.CharField(max_length=100)
+    oquvchilar_soni = models.IntegerField()
+    created_at = models.DateField()
+
+    def __str__(self):
+        return self.name
