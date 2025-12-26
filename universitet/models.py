28537a714772adebdb5678543daf20d1f49cef3c
Iqbola <Alisherovna-Iqbola0o1@gmail.com>
2025-12-26 16:36:49 +0500

    universitet_list.html va direktor_list.html Tayyorlandi

--- universitet/models.py
+++ universitet/models.py
@@ -22,4 +22,4 @@ class Direktor(models.Model):
     yaratilgan_vaqti = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.oylik
+        return f"Direktor {self.id}"
